4d552f4e4ef17b4eda4df3293fd1fd0f8dbecfb0
KrishnappaL <lakshmihk2003@gmail.com>
2024-07-05 13:50:32 +0530

    first commit

---
+++ Tweet-Trend/test/java/com/example/demo/DemoWorkshopApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+
+@SpringBootTest
+class DemoWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+		List li = new ArrayList<String> ();
+	}
+
+}
